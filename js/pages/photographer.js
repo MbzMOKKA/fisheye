60b6d082fa15eb993e1c2164dc668cc81b8ecd51
M.O.K.K.A <marbeloz.studio@gmail.com>
2023-02-13 22:38:58 +0100

    sorting feature

--- js/pages/photographer.js
+++ js/pages/photographer.js
@@ -4,7 +4,12 @@ import { mediaFactory } from '../factories/media.js';
 import { contactAddListeners } from '../utils/contactForm.js';
 import { getPhotographer, getMedias } from '../utils/apiCommunication.js';
 import { mediaAddListeners } from '../utils/mediaLightbox.js';
+import { sortingSelected, sortingOptions, sortingSelectedName, mediaList } from '../utils/domLinker.js';
 import { initCommon } from './common.js';
+import { interractibleAddEventListener } from '../utils/keyboard.js';
+import { sortingsToggle, sortingSelect } from '../utils/sortingList.js';
+
+let medias = [];
 
 //Display the photographer's infos on the photographer's profile page
 function displayPhotographerInfos(photographerModel) {
@@ -25,8 +30,31 @@ function displayPhotographerInfos(photographerModel) {
 }
 
 //Display the media uploaded by a photographer on his page
-function displayMedias(medias) {
-    medias.forEach((media) => {
+export function displayMedias() {
+    //Sort the medias
+    let sortedMedias = JSON.parse(JSON.stringify(medias));
+    switch (sortingSelectedName.textContent) {
+        case 'Date':
+            sortedMedias.sort((a, b) => {
+                return new Date(b.date) - new Date(a.date);
+            });
+            break;
+        case 'Popularité':
+            sortedMedias.sort((a, b) => {
+                return b.likes - a.likes;
+            });
+            break;
+        case 'Titre':
+            sortedMedias.sort((a, b) => {
+                if (a.title < b.title) return -1;
+                if (a.title > b.title) return 1;
+                return 0;
+            });
+            break;
+    }
+    //Replace the content of the media list
+    mediaList.textContent = '';
+    sortedMedias.forEach((media) => {
         const mediaModel = mediaFactory(media);
         mediaModel.getCardDOM();
     });
@@ -36,13 +64,17 @@ function displayMedias(medias) {
 async function initPhotographerPage() {
     const id = new URL(document.location).searchParams.get('id');
     const photographer = await getPhotographer(id);
-    const medias = await getMedias(id);
+    medias = await getMedias(id);
     const photographerModel = photographerFactory(photographer);
     displayPhotographerInfos(photographerModel);
     photographerModel.displayTotalLikes(medias);
-    displayMedias(medias);
+    displayMedias();
     contactAddListeners();
     mediaAddListeners();
+    interractibleAddEventListener(sortingSelected, sortingsToggle);
+    sortingOptions.forEach((option) => {
+        interractibleAddEventListener(option, sortingSelect);
+    });
 }
 
 initCommon();
---
+++ js/utils/sortingList.js
@@ -0,0 +1,36 @@
+//Imports
+import { sortingList, sortingExpandedList, sortingExpandedIcon, sortingOptions, sortingSelected, sortingSelectedName } from './domLinker.js';
+import { displayMedias } from '../pages/photographer.js';
+
+//Open the sorting list
+export function sortingsToggle(e) {
+    e.preventDefault();
+    if (sortingList.getAttribute('aria-expanded') == 'false') {
+        sortingList.setAttribute('aria-expanded', 'true');
+        sortingExpandedList.setAttribute('aria-hidden', 'false');
+        sortingExpandedIcon.setAttribute('src', 'assets/icons/arrow_up.svg');
+    } else {
+        sortingList.setAttribute('aria-expanded', 'false');
+        sortingExpandedList.setAttribute('aria-hidden', 'true');
+        sortingExpandedIcon.setAttribute('src', 'assets/icons/arrow_down.svg');
+        sortingSelected.focus();
+    }
+}
+
+//Select a sorting
+export function sortingSelect(e) {
+    const prevsortingName = sortingSelectedName.textContent;
+    const newsortingName = e.target.textContent;
+
+    //Permute the new selected sorting with the previous one in the list
+    sortingSelectedName.textContent = newsortingName;
+    sortingOptions.forEach((option) => {
+        if (option.textContent == newsortingName) {
+            option.textContent = prevsortingName;
+        }
+    });
+
+    //Apply the changes
+    displayMedias();
+    sortingsToggle(e);
+}
